aeb563f5934207a1c16a06804b95df03fe66c739
Tobías Girado <tobiasgirado@MacBook-Pro.local>
2023-10-02 18:17:24 -0300

    log

--- netlify/functions/webhookMeli/webhookMeli.ts
+++ netlify/functions/webhookMeli/webhookMeli.ts
@@ -3,6 +3,7 @@ import { Handler } from '@netlify/functions'
 export const handler: Handler = async (event, context) => {
   const { name = 'stranger' } = event.queryStringParameters
 
+  console.log("A la grande le puse cuca");
   return {
     statusCode: 200,
     body: JSON.stringify({
