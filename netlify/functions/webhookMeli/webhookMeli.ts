56d4183522c0a8144188bb5056632e808fb7aafe
Tobías Girado <tobiasgirado@MacBook-Pro.local>
2023-10-02 22:17:12 -0300

    returning name

--- netlify/functions/webhookMeli/webhookMeli.ts
+++ netlify/functions/webhookMeli/webhookMeli.ts
@@ -1,14 +1,8 @@
 import { Handler } from '@netlify/functions'
 
 export const handler: Handler = async (event, context) => {
-  const name = event.body?.length == 0 ?  'stranger' : event.body
-
-  console.log("A la grande le puse cucas");
   console.log(event.body);
   return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello, ${name}!`,
-    }),
+    statusCode: 200
   }
 }
